ac0cdea62d3aeb3119606694b593a5b1fd24eb15
chabdullah <chabdullah@hotmail.it>
2019-09-14 15:28:31 +0200

    Aggiunto un altro motore. Aggiunto 4 materiali invece che 3

--- Cest.ino
+++ Cest.ino
@@ -13,8 +13,8 @@ const char* password = "lasolita";  //Enter Password here
 
 
 //Motore
-Servo myservo_1;
-int pos_servo1;
+Servo myservo_1, myservo_2;
+int pos_servo1, pos_servo2;
 
 
 //Sensori materiali
@@ -49,6 +49,7 @@ void setup() {
   pinMode(inductivePin,INPUT);
   pinMode(capacitivePin,INPUT);
   myservo_1.attach(5);
+  myservo_2.attach(16);
   
   //Collegamento al router
   Serial.println();
@@ -79,30 +80,48 @@ void loop() {
       capacitiveState = valCapacitive;
       oldDistance = newDistance;
       
-      Serial.print("Materiale = ");
+      Serial.print("Elaboro tipo materiale");
+      delay(2000);
       if( inductiveState == 0 ){
         material_type = 1;
-        Serial.print( "Metallo\t" );
-        for (pos_servo1 = 0; pos_servo1 <= 180; pos_servo1 += 1) {
-          myservo_1.write(pos_servo1);              
-          delay(15);
-        }
       }else if(capacitiveState == 1){
-        material_type = 3;
-        Serial.print( "Plastica/Vetro\t");
+        material_type = 2;
       }else{
         material_type = 4;
-        Serial.print("Indifferenziata\t");
       }
-      Serial.println();
-
-      
+      Serial.println("Fatto.");
+      delay(1000);
+      ////////////////////////////////////////////////
       Serial.println("Misura peso...");
       delay(2000);
       int sum = 0;
       for(int i=0; i<25; i++)
         sum += analogRead(fsrAnalogPin);
       weight = sum/25;
+      ////////////////////////////////////////////////
+      
+      if( material_type == 1 ){
+        Serial.print( "Metallo\t" );
+        for (pos_servo1 = 0; pos_servo1 <= 180; pos_servo1 += 1) {
+          myservo_1.write(pos_servo1);              
+          delay(15);
+        }
+      }else if(material_type == 2){
+        if(weight > 230){
+            Serial.print("Vetro");
+            material_type = 3;  
+        }else{
+            Serial.print("Plastica");
+            material_type = 2;
+            for (pos_servo2 = 0; pos_servo2 <= 180; pos_servo2 += 1) {
+              myservo_2.write(pos_servo2);              
+              delay(15);
+            }
+        }
+      }else{
+        Serial.print("Indifferenziata\t");
+      }
+      Serial.println();
       Serial.print("Peso: ");
       Serial.print(weight);
       Serial.println(" g");
@@ -128,6 +147,11 @@ void loop() {
       if(material_type == 1){
         for (pos_servo1 = 180; pos_servo1 >= 0 ; pos_servo1 -= 1) {
           myservo_1.write(pos_servo1);              
+          delay(15);
+        }  
+      }else if(material_type == 2){
+        for (pos_servo2 = 180; pos_servo2 >= 0 ; pos_servo2 -= 1) {
+          myservo_2.write(pos_servo2);              
           delay(15);
         }  
       }
